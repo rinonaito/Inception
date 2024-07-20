729803c212ddd919188859b4b08cabdc1d6a1d0e
RinoNaito <rinodelevingne@gmail.com>
2024-07-20 19:14:31 +0900

    MOD: created database in the mariadb container

--- docker/mariadb/entrypoint.sh
+++ docker/mariadb/entrypoint.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
 
-# add mysql-group if NOT exist
-if ! getent group mysql; then
-  groupadd -r mysql
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+	echo "inside of the biggest if";
+
+	# add mysql-group if NOT exist
+	if ! getent group mysql; then
+	  groupadd -r mysql
+	fi
+	
+	# add mysql-user if NOT exist
+	if ! getent passwd mysql; then
+	  useradd -r -g mysql mysql
+	fi
+	
+	# set user:group as a owner of the data directory
+	chown -R mysql:mysql /var/lib/mysql
+	
+	# make /run/mysqld directory if NOT exist
+	if [ ! -d "/run/mysqld" ]; then
+	  mkdir -p /run/mysqld
+	fi
+	
+	# set user:group as a owner of the socket directory
+	chown -R mysql:mysql /run/mysqld
+	
+	if [ ! -d "/var/lib/mysql/mysql" ]; then
+	  mysql_install_db --user=mysql
+	fi
+	
+	# execute mysqld in background so as to go forward
+	cat <<EOF > /etc/query.txt
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+	mysqld --user=mysql --bootstrap < /bin/cat < /etc/query.txt
 fi
-
-# add mysql-user if NOT exist
-if ! getent passwd mysql; then
-  useradd -r -g mysql mysql
-fi
-
-# set user:group as a owner of the data directory
-chown -R mysql:mysql /var/lib/mysql
-
-# make /run/mysqld directory if NOT exist
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-fi
-
-# set user:group as a owner of the socket directory
-chown -R mysql:mysql /run/mysqld
-
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql
-fi
-
-# execute mysqld in background so as to go forward
-mysqld &
-sleep 5
-
-# execute query and create database
-mysql -u root -p ${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE}; CREATE USER ${MYSQL_USER}'@'%' IDENTIFIED BY ${MYSQL_PASSWOR}; GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@'%'; FLUSH PRIVILEGES;"
 
 exec "$@"
--- docker/wordpress/entrypoint.sh
+++ docker/wordpress/entrypoint.sh
@@ -7,13 +7,15 @@ unzip latest.zip
 service php7.4-fpm start;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-	sudo -u www-data wp core download --path=${WP_INSTALL_DIR} --locale=ja
-	sudo -u www-data wp config create \
-	    --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpassword=$MYSQL_ROOT_PASSWORD --dbhost=$WORDPRESS_DB_HOST \
-	    --force --path=${WP_INSTALL_DIR}
+	#sudo -u www-data wp core download --path=${WP_INSTALL_DIR} --locale=ja
+	wp core download --path=${WP_INSTALL_DIR} --locale=ja --allow-root
+	#sudo -u www-data wp config create \
+	wp config create \
+	    --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$WORDPRESS_DB_HOST \
+	    --force --path=${WP_INSTALL_DIR} --allow-root
 		
 fi
 
-mv wordpress/* /var/www/html/
+#mv wordpress/* /var/www/html/
 
 bash
