9e14bb190fc6ce8e967b2b293de9271d13f4cd87
RinoNaito <rinodelevingne@gmail.com>
2024-09-01 18:07:04 +0900

    created wp-config.php file

--- docker/wordpress/entrypoint.sh
+++ docker/wordpress/entrypoint.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-WP_INSTALL_DIR=/var/www/html
+WP_INSTALL_DIR=/var/www/html/
 
 wget https://wordpress.org/latest.zip
 unzip latest.zip
 service php7.4-fpm start;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-	#sudo -u www-data wp core download --path=${WP_INSTALL_DIR} --locale=ja
-	wp core download --path=${WP_INSTALL_DIR} --locale=ja --allow-root
-	#sudo -u www-data wp config create \
-	wp config create \
-	    --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$WORDPRESS_DB_HOST \
-	    --force --path=${WP_INSTALL_DIR} --allow-root
-		
-fi
 
-#mv wordpress/* /var/www/html/
+	# add root user in www-data group
+	sudo usermod -aG www-data root
+
+	# change owner of the /var/www/html directory
+	sudo chown -R www-data:www-data /var/www/html
+
+	# chenge mode of the /var/www/html directory
+	sudo chmod -R 755 /var/www/html	
+
+	# download wordpress files to the /var/www/html directory
+	wp core download --path=${WP_INSTALL_DIR} --locale=ja --debug --allow-root
+
+	# command "wp config create" only works here
+	cd ${WP_INSTALL_DIR}
+
+	# create wp-config.php if parameter is correct
+	sudo -u www-data wp --debug config create \
+		--dbname=$MYSQL_DATABASE \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD \
+		--dbhost=$WORDPRESS_DB_HOST \
+		--force \
+		--path=${WP_INSTALL_DIR}
+fi
 
 bash
